652060fef6a426528bdc5146534447e9de6a6061
Ay-355 <77558710+Ay-355@users.noreply.github.com>
2021-11-30 19:00:35 -0700

    fix brick wall offset
    Before, it would add half a brick on the left on even rows, and half on
    the right on odd rows.
    Now it will add a half brick on every other row, on both sides.
    This makes it so theis are actually the requested amount of bricks.

--- brick_wall.py
+++ brick_wall.py
@@ -36,20 +36,25 @@ def draw_brick_wall_offset(rows, cols, brick_width, brick_height, mortar_width):
                         (brick_width - mortar_width) / 2,
                         brick_height,
                     )
-                draw_rect(x * width + width / 2, y * height, brick_width, brick_height)
-            else:
-                draw_rect(x * width, y * height, brick_width, brick_height)
-                if x == rows - 1:
+                if x == rows-1:
                     draw_rect(
-                        rows * width,
+                        x * width + width / 2,
                         y * height,
                         (brick_width - mortar_width) / 2,
                         brick_height,
                     )
+                else:
+                    draw_rect(x * width + width / 2, y * height, brick_width, brick_height) 
+            else:
+                draw_rect(x * width, y * height, brick_width, brick_height)
 
 
 def draw_brick_wall_special(rows, cols, brick_width, brick_height, mortar_width):
-    ... 
+    for y in range(cols):
+        for x in range(rows):
+            brick_width, brick_height = brick_height, brick_width
+            if x % 2:
+                draw_rect(x * (brick_width + mortar_width), y * (brick_height + mortar_width), brick_width, brick_height)
 
 
 def draw_brick_wall(rows, cols, brick_width, brick_height, mortar_width):
@@ -58,5 +63,5 @@ def draw_brick_wall(rows, cols, brick_width, brick_height, mortar_width):
             draw_rect(x * (brick_width + mortar_width), y * (brick_height + mortar_width), brick_width, brick_height)
 
 
-draw_brick_wall_offset(20, 8, 50, 25, 5)
+draw_brick_wall_special(8, 8, 50, 25, 5)
 turtle.done()
