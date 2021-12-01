2cd59984eaeaae7d6bfe12d2f2f86ad85c0dddd4
Ay-355 <77558710+Ay-355@users.noreply.github.com>
2021-12-01 12:22:16 -0700

    Add special brick wall

--- brick_wall.py
+++ brick_wall.py
@@ -26,9 +26,9 @@ def draw_rect(x, y, length, height):
 def draw_brick_wall_offset(rows, cols, brick_width, brick_height, mortar_width):
     width = brick_width + mortar_width
     height = brick_height + mortar_width
-    for y in range(cols):
-        for x in range(rows):
-            if y % 2 == 0:
+    for y in range(rows):
+        for x in range(cols):
+            if y % 2:
                 if x == 0:
                     draw_rect(
                         x * width,
@@ -36,7 +36,7 @@ def draw_brick_wall_offset(rows, cols, brick_width, brick_height, mortar_width):
                         (brick_width - mortar_width) / 2,
                         brick_height,
                     )
-                if x == rows-1:
+                if x == cols - 1:
                     draw_rect(
                         x * width + width / 2,
                         y * height,
@@ -44,21 +44,31 @@ def draw_brick_wall_offset(rows, cols, brick_width, brick_height, mortar_width):
                         brick_height,
                     )
                 else:
-                    draw_rect(x * width + width / 2, y * height, brick_width, brick_height) 
+                    draw_rect(x * width + width / 2, y * height, brick_width, brick_height)
             else:
                 draw_rect(x * width, y * height, brick_width, brick_height)
 
 
-# only works if brick_width is double brick_height
-def draw_brick_wall_special(rows, cols, brick_width, brick_height, mortar_width):
+def draw_brick_wall_special(rows, cols, brick_height, mortar_width):
+    brick_width = 2 * brick_height
     for y in range(cols):
         for x in range(rows):
-            if x % 2 ==0:
+            if x % 2 == 0:
                 draw_rect(x * (brick_width + mortar_width), y * (brick_height + mortar_width), brick_width, brick_height)
             else:
                 if y % 2 == 0:
-                    draw_rect(x * (brick_width + mortar_width), y * (brick_height + mortar_width), brick_height - mortar_width/2, brick_width + mortar_width)
-                    draw_rect(x * (brick_width + mortar_width) + (brick_width + mortar_width)/2, y * (brick_height + mortar_width), brick_height - mortar_width/2, brick_width + mortar_width)
+                    draw_rect(
+                        x * (brick_width + mortar_width),
+                        y * (brick_height + mortar_width),
+                        brick_height - mortar_width / 2,
+                        brick_width + mortar_width,
+                    )
+                    draw_rect(
+                        x * (brick_width + mortar_width) + (brick_width + mortar_width) / 2,
+                        y * (brick_height + mortar_width),
+                        brick_height - mortar_width / 2,
+                        brick_width + mortar_width,
+                    )
 
 
 def draw_brick_wall(rows, cols, brick_width, brick_height, mortar_width):
@@ -67,6 +77,6 @@ def draw_brick_wall(rows, cols, brick_width, brick_height, mortar_width):
             draw_rect(x * (brick_width + mortar_width), y * (brick_height + mortar_width), brick_width, brick_height)
 
 
-draw_brick_wall_offset(8, 8, 50, 25, 5)
-# draw_brick_wall_special(8, 8, 50, 25, 5)
+# draw_brick_wall_offset(10, 5, 50, 25, 5)
+draw_brick_wall_special(8, 8, 15, 5)
 turtle.done()
