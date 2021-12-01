f6c9c576a5b85723114460a8e7950bf172b7f618
Ay-355 <77558710+Ay-355@users.noreply.github.com>
2021-11-30 22:44:38 -0700

    Special brick wall

--- brick_wall.py
+++ brick_wall.py
@@ -49,12 +49,16 @@ def draw_brick_wall_offset(rows, cols, brick_width, brick_height, mortar_width):
                 draw_rect(x * width, y * height, brick_width, brick_height)
 
 
+# only works if brick_width is double brick_height
 def draw_brick_wall_special(rows, cols, brick_width, brick_height, mortar_width):
     for y in range(cols):
         for x in range(rows):
-            brick_width, brick_height = brick_height, brick_width
-            if x % 2:
+            if x % 2 ==0:
                 draw_rect(x * (brick_width + mortar_width), y * (brick_height + mortar_width), brick_width, brick_height)
+            else:
+                if y % 2 == 0:
+                    draw_rect(x * (brick_width + mortar_width), y * (brick_height + mortar_width), brick_height - mortar_width/2, brick_width + mortar_width)
+                    draw_rect(x * (brick_width + mortar_width) + (brick_width + mortar_width)/2, y * (brick_height + mortar_width), brick_height - mortar_width/2, brick_width + mortar_width)
 
 
 def draw_brick_wall(rows, cols, brick_width, brick_height, mortar_width):
@@ -63,5 +67,6 @@ def draw_brick_wall(rows, cols, brick_width, brick_height, mortar_width):
             draw_rect(x * (brick_width + mortar_width), y * (brick_height + mortar_width), brick_width, brick_height)
 
 
-draw_brick_wall_special(8, 8, 50, 25, 5)
+draw_brick_wall_offset(8, 8, 50, 25, 5)
+# draw_brick_wall_special(8, 8, 50, 25, 5)
 turtle.done()
