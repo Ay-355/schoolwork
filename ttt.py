ee7d294c9389a9497d4f7526e55a07dc1b5d7f0b
Ay-355 <77558710+Ay-355@users.noreply.github.com>
2022-01-11 14:26:20 -0700

    Working tic tac toe

--- ttt.py
+++ ttt.py
@@ -1,54 +1,117 @@
 import turtle
 
+
 def draw_board():
-    pointer = turtle.Turtle("square")
+    box = turtle.Turtle()
+    box.hideturtle()
+    box.speed(100)
+    box.width(2)
+    box.penup()
+    box.goto(0, 2)
+    box.pendown()
+    box.forward(6)
+    box.penup()
+    box.goto(0, 4)
+    box.pendown()
+    box.forward(6)
+    box.penup()
+    box.left(90)
+    box.goto(2, 0)
+    box.pendown()
+    box.forward(6)
+    box.penup()
+    box.goto(4, 0)
+    box.pendown()
+    box.forward(6)
+    box.penup()
+
+def check_win(board: list):
+    
+    # horizontal
+    for x in board:
+        if sum(x) == 3:
+            return "square"
+        elif sum(x) == -3:
+            return "circle"
+
+    diag = board[0][2] + board[1][1] + board[2][0] 
+    if diag == 3:
+        return "square"
+    elif diag == -3:
+        return "circle"
+
+    diag = sum(board[i][i] for i in range(3))
+    if diag == 3:
+        return "square"
+    elif diag == -3:
+        return "circle"
+
+    # vertical
+    for i in range(3):
+        vert = board[0][i] + board[1][i] + board[2][i]
+        if vert == 3:
+            return "square"
+        elif vert == -3:
+            return "circle"
+ 
+    # tie
+    for i in board:
+        if not all([ v !=  0 for v in i ]):
+            break
+    else:
+        return "tie"
     
 
-def check_win():
-    ...
-
-# 1=circle, 2=square
-def stamp_shape(x, y, pointer):
+# -1 = circle, 1 = square
+def stamp_shape(x: float, y: float, pointer: turtle.Turtle):
     # OOP is so much better than globals
     global turn, board
-    pointer.shape("square" if turn % 2 == 0 else "circle")
+    pointer.shape("square" if turn == 1 else "circle")
     xr, yr = None, None
-    if round(x) in [0, 1, 2]:
+    if 0 < x < 2:
         xr = 0
-    elif round(x) in [2, 3, 4]:
+        xa = 1
+    elif 2 < x < 4:
         xr = 1
-    elif round(x) in [4, 5, 6]:
+        xa = 3
+    elif 4 < x < 6:
         xr = 2
+        xa = 5
 
-    if round(y) in [0, 1, 2]:
+    if 0 < y < 2:
         yr = 0
-    elif round(y) in [2, 3, 4]:
+        ya = 1
+    elif 2 < y < 4:
         yr = 1
-    elif round(y) in [4, 5, 6]:
+        ya = 3
+    elif 4 < y < 6:
         yr = 2
+        ya = 5
 
-    if board[xr][yr] == 1:
-        print("SQUARE ALREADY USED")
+    if board[xr][yr] != 0:
+        print("SPOT ALREADY USED")
         return
     else:
-        board[xr][yr] = 1
-
-    pointer.goto(x, y)
+        board[xr][yr] = turn
+ 
+    pointer.goto(xa, ya)
     pointer.stamp()
-    turn += 1
-
+    turn = 1 if turn == -1 else -1
+    if winner := check_win(board):
+        print(winner)
 
 def main():
     global turn, board
-    turn = 0
+    turn = 1
     screen = turtle.Screen()
     screen.setup(600, 600)
-    screen.setworldcoordinates(0,0,6,6)
+    screen.setworldcoordinates(0, 0, 6, 6)
     board = [
-            [0, 0, 0],
-            [0, 0, 0],
+            [0, 0, 0], 
+            [0, 0, 0], 
             [0, 0, 0]
-        ]
+            ]
+
     screen.clear()
     pointer = turtle.Turtle()
     pointer.hideturtle()
@@ -57,10 +120,13 @@ def main():
     pointer.pensize(15)
     pointer.speed(0)
 
+    draw_board()
+
     screen.onclick(lambda x, y: stamp_shape(x, y, pointer))
     screen.onkey(main, "n")
     screen.listen()
     screen.mainloop()
+
 
 if __name__ == "__main__":
     main()
