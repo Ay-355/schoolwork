82165010d02f4b5f5c45569cbfc87ef06acb4b15
Ay-355 <77558710+Ay-355@users.noreply.github.com>
2022-02-15 18:41:31 -0700

    Bouncing

--- breakout.py
+++ breakout.py
@@ -12,24 +12,35 @@ def move(direction: str, paddle: turtle.Turtle, screen: turtle.Screen):
     screen.update()
 
 
-def check_collision(ball: turtle.Turtle, paddle: turtle.Turtle):
+def check_collision(ball: turtle.Turtle, paddle: turtle.Turtle, boxes: list, box: turtle.Turtle):
+    # ball and paddle
     if ball.ycor() < 0.3 and ball.xcor() > paddle.xcor() - 0.65 and ball.xcor() < paddle.xcor() + 0.55:
         ball.dy *= -1
     if ball.ycor() < -0.2:
         ball.goto(5, 5)
         time.sleep(1)
 
+    m = {11.5: 0, 11: 1, 10.5: 2}
+    if ball.ycor() > 10.3:
+        # if (b := boxes[int(ball.ycor() - 11.5)][int(ball.xcor() - 0.45)]) != "done":
+        if (b := boxes[m[round(ball.ycor() * 2)/2]][int(ball.xcor() - 0.45)]) != "done":
+            box.clearstamp(b)
+            boxes[m[round(ball.ycor() * 2)/2]][int(ball.xcor() - 0.45)] = "done"
+            ball.dy *= -1
+ 
 
-def move_ball(ball: turtle.Turtle, screen: turtle.Screen, paddle: turtle.Turtle):
+def move_ball(ball: turtle.Turtle, screen: turtle.Screen, paddle: turtle.Turtle, boxes: list, box: turtle.Turtle):
     ball.setx(ball.xcor() + ball.dx)
     ball.sety(ball.ycor() + ball.dy)
+    # Bounce sides 
     if ball.xcor() > 9.9 or ball.xcor() < 0:
         ball.dx *= -1
-    if ball.ycor() > 11.9:
+    # Bounce top 
+    if ball.ycor() > 11.7:
         ball.dy *= -1
     screen.update()
-    check_collision(ball, paddle)
-    screen.ontimer(lambda: move_ball(ball, screen, paddle), 1)
+    check_collision(ball, paddle, boxes, box)
+    screen.ontimer(lambda: move_ball(ball, screen, paddle, boxes, box), 1)
 
 
 def main():
@@ -40,8 +51,6 @@ def main():
     screen.bgcolor("#000000")
 
     boxes = []
-    for _ in range(3):
-        boxes.append([True] * 10)
 
     paddle = turtle.Turtle()
     paddle.goto(5, 0)
@@ -66,18 +75,20 @@ def main():
     box.up()
     box.shapesize(1, 4)
 
-    for y in [11.5, 11, 10.5]:
+    for i, y in enumerate([11.5, 11, 10.5]):
+        boxes.append([])
         for x in range(10):
             box.color(random.choice(["blue", "green", "yellow"]))
             box.goto(x + 0.45, y)
-            box.stamp()
+            boxes[i].append(box.stamp())
+
 
     screen.onkeypress(lambda: move("left", paddle, screen), "Left")
     screen.onkeypress(lambda: move("right", paddle, screen), "Right")
-    screen.onclick(lambda x, y: print(x, y))
+    screen.onclick(print)
     screen.tracer(0)
 
-    move_ball(ball, screen, paddle)
+    move_ball(ball, screen, paddle, boxes, box)
 
     screen.listen()
     screen.mainloop()
