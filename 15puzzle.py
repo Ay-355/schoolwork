45f8bd41a859b16590825263a167a1e40a50e4aa
Ay-355 <77558710+Ay-355@users.noreply.github.com>
2022-03-22 18:29:38 -0700

    basic drawing and moving

--- 15puzzle.py
+++ 15puzzle.py
@@ -2,7 +2,7 @@ import random
 import turtle
 
 
-def draw_board():
+def draw_board(screen: turtle.Screen):
     pointer = turtle.Turtle(visible=False)
     pointer.speed(0)
     pointer.color("#425CCD")
@@ -19,9 +19,11 @@ def draw_board():
     pointer.left(90)
     for i in range(1, 4):
         line(i, -0.04)
+    screen.update()
 
 
 def draw_numbers(pointer: turtle.Turtle, board: list):
+    pointer.clear()
     for i in range(4):
         for j in range(4):
             pointer.up()
@@ -32,8 +34,31 @@ def draw_numbers(pointer: turtle.Turtle, board: list):
                 pointer.write(cur, align="center", font=("Arial", 34, "normal"))
 
 
-def check_win():
-    ...
+def swap(board: list, empty: tuple, spot_y: int, spot_x: int):
+    # need to check edges
+    ok = True
+    if ok:
+        board[empty[1]][empty[0]], board[spot_y][spot_x] = board[spot_y][spot_x], board[empty[1]][empty[0]]
+
+
+def move(board: list, direction: str, text: turtle.Turtle):
+    for i in range(4):
+        for j in range(4):
+            if board[j][i] == 0:
+                empty = (i, j)
+    if direction == "u": swap(board, empty, empty[1] - 1, empty[0])
+    if direction == "d": swap(board, empty, empty[1] + 1, empty[0])
+    if direction == "l": swap(board, empty, empty[1], empty[0] - 1)
+    if direction == "r": swap(board, empty, empty[1], empty[0] + 1)
+
+    draw_numbers(text, board)
+    if board == [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 0]]:
+        print("done")
+
+
+def move_click(x, y):
+    ix = round(x) - 1
+    iy = round(y) - 1
 
 
 def main():
@@ -42,16 +67,24 @@ def main():
     screen.setworldcoordinates(0, 0, 4, 4)
     screen.title("15 Puzzle")
     screen.bgcolor("#101113")
+    screen.tracer(0, 0)
 
     text = turtle.Turtle(visible=False)
     text.speed(0)
     text.color("#425CCD")
 
-    # final
     final_board = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 0]]
-    draw_board()
-    draw_numbers(text, final_board)
+    board = [i for j in final_board for i in j]
+    random.shuffle(board)
+    board = [board[i : i + 4] for i in range(0, len(board), 4)]
+    draw_board(screen)
+    draw_numbers(text, board)
 
+    screen.onkey(lambda: move(board, "u", text), "Up")
+    screen.onkey(lambda: move(board, "d", text), "Down")
+    screen.onkey(lambda: move(board, "l", text), "Left")
+    screen.onkey(lambda: move(board, "r", text), "Right")
+    screen.onclick(lambda x, y: move_click(x, y))
     screen.onclick(print)
     screen.listen()
     screen.mainloop()
